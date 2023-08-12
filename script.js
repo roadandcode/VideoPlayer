f5e4533d0c057a8bb1397db07b45c630992681a1
roadandcode <116621969+roadandcode@users.noreply.github.com>
2023-08-12 16:54:27 +0530

    change

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 let player
 function onYouTubeIframeAPIReady(){
     console.log("API is loaded")
+    player = YT.player("player",{
+        height: 500,
+        width:900,
+        videoId:"qLE5DH4b3vI",
+        playerVars:{
+            playersinline :1,
+            autoplay:1,
+            controls:1
+
+        }
+        // events:{
+        //     onReady:onPlayerReady,
+        //     OnStateChange: onPlayerStateChange
+        // }
+    })
 }
